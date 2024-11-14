ebd1109e5d08503194aaf56a1879c41af4a65a68
prachi <prachiavhad2004@gmail.com>
2024-11-14 21:09:42 +0530

    First Commit

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container, Header, Button } from '../styles'; // Import styled-components
+
+// Define the props type for the component
+interface MyComponentProps {
+  title: string;
+}
+
+const MyComponent: React.FC<MyComponentProps> = ({ title }) => {
+  return (
+    <Container>
+      <Header>{title}</Header>
+      <p>Name: Prachi Avhad</p>
+      <p>Roll No: 10390</p>
+      <p>Batch: D</p>
+      <p>Branch: Electronics and Computer Science</p>
+    
+    </Container>
+  );
+};
+
+export default MyComponent;
